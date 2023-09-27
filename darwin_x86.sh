0e82453d1a9f87223b745e64969a318f97819426
hyundong <hyulim@student.42seoul.kr>
2023-09-27 15:04:37 +0900

    add npm install, clang-format config

---
+++ darwin_x86.sh
@@ -0,0 +1,49 @@
+NVIM_LOCAL_PATH=$HOME/.nvim_local/
+#NVIM_LOCAL_PATH=$HOME/.local/
+
+TS=`date +%s`
+
+/bin/mv $NVIM_LOCAL_PATH ${NVIM_LOCAL_PATH}_${TS}.bak
+/bin/mv $HOME/nvim-macos $HOME/nvim-macos_${TS}.bak
+/bin/mv $HOME/.cache/nvim $HOME/.cache/nvim_${TS}.bak
+
+/bin/mkdir -p $NVIM_LOCAL_PATH/bin
+/bin/mkdir -p $NVIM_LOCAL_PATH/include
+/bin/mkdir -p $NVIM_LOCAL_PATH/lib
+/bin/mkdir -p $NVIM_LOCAL_PATH/share
+
+#mv $NVIM_LOCAL_PATH/share/nvim $NVIM_LOCAL_PATH/share/nvim_${TS}.bak
+#mv $NVIM_LOCAL_PATH/state/nvim $NVIM_LOCAL_PATH/state/nvim_${TS}.bak
+
+cd $HOME \
+	&& /bin/mkdir -p nvim_tmp \
+	&& cd nvim_tmp
+
+curl -L https://nodejs.org/dist/v18.18.0/node-v18.18.0-darwin-x64.tar.gz -o node.tar.gz\
+	&& /bin/mkdir node_tmp \
+	&& tar xzvf node.tar.gz -C node_tmp\
+	&& /bin/mv node_tmp/*/* node_tmp/ \
+	&& /bin/cp -af node_tmp/bin $NVIM_LOCAL_PATH \
+	&& /bin/cp -af node_tmp/include $NVIM_LOCAL_PATH \
+	&& /bin/cp -af node_tmp/lib $NVIM_LOCAL_PATH \
+	&& /bin/cp -af node_tmp/share $NVIM_LOCAL_PATH
+
+#curl -L -O https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
+curl -L -O https://github.com/neovim/neovim/releases/download/v0.9.2/nvim-macos.tar.gz \
+	&& xattr -c ./nvim-macos.tar.gz \
+	&& tar xzvf nvim-macos.tar.gz \
+	&& /bin/mv nvim-macos $HOME/nvim-macos
+
+/bin/mv $HOME/.config/nvim $HOME/.config/nvim_${TS}.bak
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim $HOME/.config/nvim
+git clone https://github.com/taeng42/nvim-config.git $HOME/.config/nvim/lua/user \
+	&& cp -af $HOME/.config/nvim/lua/user/lsp/config/.clang-format $HOME
+
+/bin/rm $NVIM_LOCAL_PATH/bin/glow
+curl -L -o glow.tar.gz https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_Darwin_x86_64.tar.gz && tar xzvf glow.tar.gz && mv glow $NVIM_LOCAL_PATH/bin/glow 
+#https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_Darwin_arm64.tar.gz
+#https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_1.5.1_amd64.deb
+
+
+
+rm -rf $HOME/nvim_tmp
---
+++ install.sh
@@ -0,0 +1,33 @@
+ARCH="`uname -m`"
+
+if [[ "$ARCH" == "arm"* ]]; then
+	echo "ARM!"
+	case "$OSTYPE" in
+	darwin*)
+		echo "MacOS"
+		;;
+	linux*)
+		echo "LINUX"
+		;;
+	win32 | msys* | cygwin*)
+		echo "WINDOWS"
+		;;
+	esac
+elif [[ "$ARCH" == "x86_64" ]]; then
+	echo "x86_64"
+	case "$OSTYPE" in
+	darwin*)
+		echo "MacOS"
+		bash darwin_x86.sh
+		;;
+	linux*)
+		echo "LINUX"
+		;;
+	win32 | msys* | cygwin*)
+		echo "WINDOWS"
+		;;
+	esac
+else
+	echo "unknown arch: $ARCH - $OSTYPE"
+fi
+
